89918d3b218c846149932861de5eda3979dabc68
awolter27 <awolter@butler.edu>
2023-04-01 11:10:29 -0400

    Advanced setup for index.js, movies.js

---
+++ models/Movies.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const moviesSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: [true, 'You must enter a title']
+        },
+        director: {
+            type: String,
+            required: [true, 'You must enter a director']
+        },
+        runtime: {
+            type: Number,
+            required: [true, 'You must enter a release date'],
+            defeault: 0
+        },
+        rottenTomatoes: {
+            type: Number,
+            default: 0
+        },
+        releaseDate: {
+            type: String,
+            required: [true, 'You must enter a release date']
+        },
+        genre: {
+            type: String,
+            required: [true, 'You must enter a genre']
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Movies = mongoose.model('movie', moviesSchema);
+
+module.exports = Movies;
---
+++ models/index.js
@@ -0,0 +1,5 @@
+require('../config/connection');
+
+module.exports = {
+    movies: require('./Movies')
+}
