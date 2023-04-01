aa40440b8f3514e356042a26fb6f4f74e9399e1b
awolter27 <awolter@butler.edu>
2023-04-01 10:40:34 -0400

    Initial setup for MVC, Advanced setup for .gitignore, server.js, Minor updates to package.json

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+const methodOverride = require('method-override');
+
+const moviesController = require('./controllers/movies.js');
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.use(methodOverride('_method'));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/movies', moviesController);
+
+app.get('/', (req, res) => {
+    res.render('home.ejs');
+})
+
+app.get('/*', (req, res) => {
+    res.render('404.ejs');
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
